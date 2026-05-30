d502b2fcaded2ea091fd6b764ed8ea1e32712466
Juan Pablo Bustamante <juan.bustamante.f@ug.uchile.cl>
2026-05-29 22:17:37 -0400

    feat: multiples huertos por usuario con nombre, guardables y editables.

--- huerto/forms.py
+++ huerto/forms.py
@@ -2,6 +2,12 @@ from django import forms
 from .models import Planta
 
 class PlanificadorForm(forms.Form):
+    nombre = forms.CharField(
+        label='Nombre de tu Huerto', 
+        max_length=150,
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'style': 'margin-bottom:15px; width: 100%;', 'placeholder': 'Ej. Mi primer huerto'})
+    )
     ancho_terreno = forms.FloatField(
         label='Ancho del terreno (m)', 
         min_value=0.1,
